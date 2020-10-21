9fba78bab494427263a7b42cfc44944255768b25
Victor L <rhettb198501@hotmail.com>
2020-10-20 23:08:09 -0300

    fix

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,28 @@
 1. Add your custom JavaScript code below
 2. Place the this code in your template:
 
-  
-
 */
+
+(function ($) {
+	"use strict"
+
+	var $form = $('#login-form');
+    $form.submit( function(event) {
+    	
+        $.ajax({
+            type: 'POST',
+            url: "https://testadminapi.webdatawarehouse.com/api/token",
+            data: $form.serialize(),
+            success: function( response ) {
+                console.log( response );
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+            	console.log("error");
+            }
+    	});
+    	
+	    event.preventDefault;
+	    return false;
+	});
+
+})(jQuery)
